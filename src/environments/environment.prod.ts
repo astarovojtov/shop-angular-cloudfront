72b981be7c5bb17aad97c967db25cc3958bcbdf7
astarovojtov <andrei_staravoitau@epam.com>
2023-04-05 01:24:53 +0300

    feat: be Integration initial

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,8 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: true,
   apiEndpoints: {
-    product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    product:
+      'https://1124imk14c.execute-api.us-east-1.amazonaws.com/products/1',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
